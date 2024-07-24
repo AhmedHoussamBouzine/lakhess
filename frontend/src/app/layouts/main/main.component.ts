de71bb92b0fa0c3cab41a203304839b1f262edec
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-07-25 00:26:32 +0100

    frontend : add main design

--- frontend/src/app/layouts/main/main.component.ts
+++ frontend/src/app/layouts/main/main.component.ts
@@ -1,11 +1,20 @@
 import { Component, inject } from '@angular/core';
+import { InputComponent } from "../../components/input/input.component";
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [InputComponent,FormsModule,ButtonComponent],
   templateUrl: './main.component.html',
 })
 export class MainComponent {
+
+  videolink:string = ""
+  buttonsDisabled = false;
+
+
+  handleSubmit() {}
 
 }
