7fbe25615cbc99a063639280974857c4bf2b827e
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-07-24 22:29:54 +0100

    frontend : add basic design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
