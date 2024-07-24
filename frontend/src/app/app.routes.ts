fae44ab472ce117a4b1af9760e3424a65944257d
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-07-24 22:35:12 +0100

    frontend : fix routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './layouts/main/main.component';
+import { ResultComponent } from './layouts/result/result.component';
 
 export const routes: Routes = [
-  { path : "main", component: MainComponent},
-  { path : "result", component: MainComponent},
+  {
+    path: "",
+    redirectTo: "/main",
+    pathMatch: "full",
+  },
+  { path: "main", component: MainComponent },
+  { path: "result", component: ResultComponent },
 ];
--- frontend/src/app/layouts/result/result.component.ts
+++ frontend/src/app/layouts/result/result.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './result.component.html',
-  styleUrl: './result.component.scss'
 })
 export class ResultComponent {
 
